a543f7e5685dbd8460ea2a777a0da24980f2ade6
samy polcherrafael <sp270081@dphnpct77.extra.cea.fr>
2023-03-16 11:14:56 +0100

    bin with -jar

--- bin/BeamSpot.sh
+++ bin/BeamSpot.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_DIR=`dirname $0`
 
-java -Dsun.java2d.pmoffscreen=false -Xmx2048m -Xms1024m -Xdiag -cp "$SCRIPT_DIR/../target/BeamSpotMerged-0.0.1-jar-with-dependencies.jar" org.clas.beamspot.analysis.DCbeamSpot $*
+#java -Dsun.java2d.pmoffscreen=false -Xmx2048m -Xms1024m -Xdiag -cp "$SCRIPT_DIR/../target/BeamSpotMerged-0.0.1-jar-with-dependencies.jar" org.clas.beamspot.analysis.DCbeamSpot $*
+java -jar ~/software/eclipse/eclipse-workspace/beam-spot/target/beam-spot-0.0.1-SNAPSHOT-jar-with-dependencies.jar $*
