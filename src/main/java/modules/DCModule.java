5723561b87bbb9f406714047e0bf42ce5f0fd383
samy polcherrafael <sp270081@dphnpct77.extra.cea.fr>
2023-03-31 22:39:29 +0200

    debug

--- src/main/java/modules/DCModule.java
+++ src/main/java/modules/DCModule.java
@@ -204,6 +204,8 @@ public class DCModule  extends Module {
               this.getHistos().get("distribution").getH1F("vz").fill(track.vz());
               this.getHistos().get("distribution").getH1F("phi").fill(phi);
               this.getHistos().get("z_phi").getH2F("z_phi_"+bin).fill(track.vz(), phi);
+              
+              System.out.println("cc");
           }
       }
     }
