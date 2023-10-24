0cce834ee4a0f610f627fae8acb8ba519e60135b
Samy Polcher Rafael <samy.polcher-rafael@universite-paris-saclay.fr>
2023-10-24 08:40:32 +0200

    closest apporach to xb, yb

--- src/main/java/modules/DCModule.java
+++ src/main/java/modules/DCModule.java
@@ -236,8 +236,8 @@ public class DCModule  extends Module {
               this.getHistos().get("distribution").getH1F("phi").fill(phi);
               this.getHistos().get("distribution").getH1F("xb").fill(track.xb());
               this.getHistos().get("distribution").getH1F("yb").fill(track.yb());
-              this.getHistos().get("distribution").getH1F("vx").fill(track.vx()-track.xb());
-              this.getHistos().get("distribution").getH1F("vy").fill(track.vy()-track.yb());
+              this.getHistos().get("distribution").getH1F("vx").fill(vertex.x()-track.xb());
+              this.getHistos().get("distribution").getH1F("vy").fill(vertex.y()-track.yb());
 
               this.getHistos().get("z_phi").getH2F("z_phi_"+thetaBin).fill(track.vz(), phi);
               this.getHistos().get("z_slice").getH1F("slice_"+ thetaBin+"_"+phiBin).fill(track.vz());
